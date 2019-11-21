160c68126e00c2ba0af8b1827382a9617e530547
Robin7374 <7374robin@gmail.com>
2019-11-21 11:32:36 -0700

    broken into smaller compents and styled menu items

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -4,9 +4,10 @@ import '../menu-item/menu-item.scss';
 const MenuItem = ({ title, imageUrl, size }) => {
   const menuItemStyle = { backgroundImage: `url(${imageUrl})` };
   return (
-    <div style={menuItemStyle} className={` ${size} menu-item`}>
+    <div className={` ${size} menu-item`}>
+      <div style={menuItemStyle} className='background-image'></div>
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>Shop</span>
       </div>
     </div>
