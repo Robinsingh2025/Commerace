bd9c65cd71a40281a52ce0a27df472ccfbbdfd73
Robin7374 <7374robin@gmail.com>
2019-11-25 22:08:44 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import HomePage from './pages/homepage/Homepage';
-import './App.css';
-import ShopPage from './pages/shop/shop';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/Homepage";
+import "./App.css";
+import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
